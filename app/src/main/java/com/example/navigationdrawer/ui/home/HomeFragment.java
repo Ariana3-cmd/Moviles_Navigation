8432d3b845f64648588f57ab330d67b11a631f0c
Alumno FIA - L7 <alum.fial7@lim.upeu.edu.pe>
2022-09-15 16:29:57 -0500

    Tarea

---
+++ app/src/main/java/com/example/navigationdrawer/ui/home/HomeFragment.java
@@ -0,0 +1,50 @@
+package com.example.navigationdrawer.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.navigationdrawer.R;
+import com.example.navigationdrawer.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+   Button boton;
+   EditText editText;
+    View view;
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+       editText = view.findViewById(R.id.edtNumber);
+        boton = view.findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            double n = Double.parseDouble(editText.getText().toString().trim());
+            Toast.makeText(getContext(),""+Math.pow(n,0.33333333),Toast.LENGTH_LONG).show();
+         }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
